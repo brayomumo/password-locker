53a30116080876edcf42960cccd3d7c5eeb50f5b
brayomumo <brayomumo5@gmail.com>
2019-07-21 11:37:38 +0300

    test for displaying credentials

--- credential_test.py
+++ credential_test.py
@@ -21,6 +21,9 @@ class TestCredentials(unittest.TestCase):
         self.new_credential.save_credential()
         self.assertEqual(len(Credentials.credentials_list),1)
 
+    def tearDown(self):
+        Credentials.credentials_list = []
+
     def test_delete_credential(self):
         self.new_credential.save_credential()
         test_credential = Credentials("twitter", "brayomull7", "mumo")
@@ -28,6 +31,21 @@ class TestCredentials(unittest.TestCase):
 
         self.new_credential.delete_credential()
         self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_find_credential_by_accountname(self):
+        '''
+        search for credential using username
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credentials("instagram", "brayomumo", "mumoo")
+        test_credential.save_credential()
+
+        credential_exist = Credentials.find_by_accountname("instagram")
+        self.assertTrue(credential_exist)
+
+    def test_display_credentials(self):
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+
 
 
 
