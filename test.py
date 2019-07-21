66154eb148643f9748f46fb7b6b4d8bbb7ce45c6
brayomumo <brayomumo5@gmail.com>
2019-07-21 07:27:38 +0300

    Test for saving users

--- test.py
+++ test.py
@@ -1,3 +1,31 @@
 import unittest
-import password
+from password import User
+import credentials
 
+class TestUser(unittest.TestCase):
+    '''
+    Test class which defines test cases for the user class functions
+    '''
+    def setUp(self):
+        '''
+        setup method to run before each case
+        '''
+        self.new_user = User("brian", "mumo")
+
+    def test_init(self):
+        '''
+        test to check if the object is initialized correctly
+        '''
+        self.assertEqual(self.new_user.username,"brian")
+        self.assertEqual(self.new_user.password, "mumo")
+
+    def test_save_user(self):
+        '''
+        test if the user is being saved 
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
+    if __name__ == '__main__':
+        unittest.main()
