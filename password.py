52162f977c4d09ecaff81f0316b72671f2e5324f
brayomumo <brayomumo5@gmail.com>
2019-07-21 08:16:13 +0300

    add userlist

--- password.py
+++ password.py
@@ -1,6 +1,12 @@
 class User:
 
-    user = []
+    #store the user 
+    user_list = []
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+    def save_user(self):
+        User.user_list.append(self)
