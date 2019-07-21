09e252a8e5a3a2ab7665741d59268119471ac034
brayomumo <brayomumo5@gmail.com>
2019-07-21 09:21:55 +0300

    import random module

--- credentials.py
+++ credentials.py
@@ -1,7 +1,16 @@
+import random
 class Credentials:
 
-    def __init__(self, account_name, account_password):
+
+    credentials_list = []
+
+
+    def __init__(self, account_name, account_uName, account_password):
         self.account_name = account_name
         self.account_password = account_password
+        self.account_uName = account_uName
+
+    
+    
     
     
